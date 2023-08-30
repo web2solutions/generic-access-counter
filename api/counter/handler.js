dc1d195987715ded296bd54c54a140a84e404b36
José Eduardo Almeida <web2solucoes@gmail.com>
2023-08-30 16:02:17 -0300

    Update handler.js

--- api/counter/handler.js
+++ api/counter/handler.js
@@ -1,19 +1,28 @@
 export default function handler(request, response) {
     const { createCanvas } = require("canvas");
 
-    // Dimensions for the image
-    const width = 200;
-    const height = 100;
+    const width = 1200;
+    const height = 627;
 
-    // Instantiate the canvas object
+    // Add post object with the content to render
+    const post = {
+    title: "Draw and save images with Canvas and Node"
+    }
+
     const canvas = createCanvas(width, height);
     const context = canvas.getContext("2d");
 
-    // Fill the rectangle with purple
     context.fillStyle = "#764abc";
     context.fillRect(0, 0, width, height);
 
-    // Write the image to file
+    // Set the style of the test and render it to the canvas
+    context.font = "bold 70pt 'PT Sans'";
+    context.textAlign = "center";
+    context.fillStyle = "#fff";
+    // 600 is the x value (the center of the image)
+    // 170 is the y (the top of the line of text)
+    context.fillText(post.title, 600, 170);
+
     const buffer = canvas.toBuffer("image/png");
     response.status(200).json({ data: buffer });
 };
