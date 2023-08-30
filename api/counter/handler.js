a76b6085c6f0b265287b5dea2ea330dab316da3a
José Eduardo Almeida <web2solucoes@gmail.com>
2023-08-30 15:37:10 -0300

    Update handler.js

--- api/counter/handler.js
+++ api/counter/handler.js
@@ -1,3 +1,19 @@
 export default function handler(request, response) {
-    response.status(200).json({ foo: 'bar' });
+    const { createCanvas } = require("canvas");
+
+    // Dimensions for the image
+    const width = 200;
+    const height = 100;
+
+    // Instantiate the canvas object
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext("2d");
+
+    // Fill the rectangle with purple
+    context.fillStyle = "#764abc";
+    context.fillRect(0, 0, width, height);
+
+    // Write the image to file
+    const buffer = canvas.toBuffer("image/png");
+    response.status(200).json({ data: buffer });
 };
