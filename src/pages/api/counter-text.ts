20094a5d153a3ea009f1d28444943be2a64ed933
José Eduardo Almeida <web2solucoes@gmail.com>
2023-08-30 19:55:08 -0300

    Update counter-text.ts

--- src/pages/api/counter-text.ts
+++ src/pages/api/counter-text.ts
@@ -40,7 +40,7 @@ export default async function handler(
   context.textAlign = "left";
   context.fillStyle = "#fff";
   context.fillText(`You are the visitor #${counter} since ${since}`, 150, 5);
-  context.fillText(`Counter made with vercel serverless`, 170, 5);
+  context.fillText(`Made with vercel serverless. Click to check the source.`, 170, 5);
   
   const buffer = await canvas.toBuffer('image/jpeg')
   res.status(200).setHeader('Content-Type', 'image/jpeg').send(buffer)
